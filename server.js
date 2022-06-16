7532cc04962a538923bfa28f8c8896f38b96588f
Carlos Gámez <hardpx@gmail.com>
2022-06-16 18:56:22 -0500

    added some get requests

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 2121;
 
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST");
-  res.send("This is a response");
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST");
+//   res.send("This is a response");
+// });
+
+app.get("/21savage", (req, res) => {
+  res.send(`2121212121212121`);
+});
+
+app.get("/kendrick", (req, res) => {
+  res.send("I'm so fuckin' sick and tired of the Photoshop");
+});
+
+app.get("*", (req, res) => {
+  res.send("Path not found");
 });
 
 app.listen(PORT, () => {
