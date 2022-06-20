ac7c36267c08dfd4ef9f5a700857c27a41f11405
Carlos Gámez <hardpx@gmail.com>
2022-06-19 19:48:14 -0500

    added a get request with query strings

--- server.js
+++ server.js
@@ -16,12 +16,24 @@ app.get("/r/:rappers", (req, res) => {
   res.send(`This is ${rappers}'s page`);
 });
 
+app.get("/r/:rappers/:lyrics", (req, res) => {
+  const { rappers, lyrics } = req.params;
+  res.send(
+    `This is ${rappers}'s page, currently looking at lyrics for ${lyrics}`
+  );
+});
+
 app.get("/21savage", (req, res) => {
   res.send(`2121212121212121`);
 });
 
 app.get("/kendrick", (req, res) => {
   res.send("I'm so fuckin' sick and tired of the Photoshop");
+});
+
+app.get("/search", (req, res) => {
+  const { q } = req.query;
+  res.send(`Search results for: ${q}`);
 });
 
 app.get("*", (req, res) => {
