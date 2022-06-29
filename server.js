bab9bab7e8d04cab8d4fb06682baa3412f8771c4
Carlos Gámez <hardpx@gmail.com>
2022-06-29 11:22:53 -0500

    added random page that displays a random number

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.set("views", path.join(__dirname, "/views"));
 // });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index.ejs");
+});
+
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random.ejs", { rand: num });
 });
 
 app.get("/r/:rappers", (req, res) => {
